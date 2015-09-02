4cb0cf3d72491ee1d554362b1fbaf1d71ee35068
Philipp Crocoll <crocoapps@gmail.com>
2015-09-02 22:32:28 +0200

    added files to simplify build

---
+++ src/Kp2aBusinessLogic/Io/DropboxFileStorageKeysDummy.cs
@@ -0,0 +1,13 @@
+namespace keepass2android.Io
+{
+	public partial class DropboxFileStorage
+	{
+		private const string AppKey = "";
+		private const string AppSecret = "";
+	}
+	public partial class DropboxAppFolderFileStorage
+	{
+		private const string AppKey = "";
+		private const string AppSecret = "";
+	}
+}
