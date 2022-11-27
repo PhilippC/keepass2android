d31efe7177770e3fc9ff658eb1abe3610b838c25
tenzap <fabstz-it@yahoo.fr>
2022-11-27 22:03:31 +0100

    build-xamarin.* scripts: rename target to keepass2android-app

--- src/build-scripts/build-xamarin.bat
+++ src/build-scripts/build-xamarin.bat
@@ -11,6 +11,6 @@ cd ..
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
 
-msbuild KeePass.sln /target:keepass2android /p:BuildProjectReferences=true /p:Configuration="Debug" /p:Platform="Any CPU"
+msbuild KeePass.sln /target:keepass2android-app /p:BuildProjectReferences=true /p:Configuration="Debug" /p:Platform="Any CPU"
 
 cd build-scripts
