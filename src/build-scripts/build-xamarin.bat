c8e1d7ea6419a975d298181016ea3afc16ff5b95
tenzap <fabstz-it@yahoo.fr>
2022-11-28 19:30:56 +0100

    /build-scripts/build-xamarin.bat: build also APK
    Tested with VS Community 2019

--- src/build-scripts/build-xamarin.bat
+++ src/build-scripts/build-xamarin.bat
@@ -11,6 +11,8 @@ cd ..
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86_amd64
 
-msbuild KeePass.sln /target:keepass2android-app /p:BuildProjectReferences=true /p:Configuration="Debug" /p:Platform="Any CPU"
+msbuild KeePass.sln /target:keepass2android-app /p:BuildProjectReferences=true /p:Configuration="Debug" /p:Platform="Any CPU" /p:AndroidBuildApplicationPackage=True
 
 cd build-scripts
+
+echo apk can be found in src\keepass2android\bin\Release
