08d6fb226fcb5b3f6badfd71abc7def3e0977cae
tenzap <fabstz-it@yahoo.fr>
2022-12-01 18:32:38 +0100

    build-java.bat: prefix call to gradlew.bat with 'call'
    Otherwise, the batch file stops at the first gradlew call.

--- src/build-scripts/build-java.bat
+++ src/build-scripts/build-java.bat
@@ -1,20 +1,20 @@
 cd ..\java\JavaFileStorageTest-AS
-./gradlew clean assemble
+call ./gradlew clean assemble
 cd ..\..\build-scripts
 
 cd ..\java\KP2ASoftkeyboard_AS
-./gradlew clean assemble
+call ./gradlew clean assemble
 cd ..\..\build-scripts
 
 cd ..\java\Keepass2AndroidPluginSDK2
-./gradlew clean assemble
+call ./gradlew clean assemble
 cd ..\..\build-scripts
 
 cd ..\java\KP2AKdbLibrary
-./gradlew clean assemble
+call ./gradlew clean assemble
 cd ..\..\build-scripts
 
 cd ..\java\PluginQR
-./gradlew clean assemble
+call ./gradlew clean assemble
 
 cd ..\..\build-scripts
