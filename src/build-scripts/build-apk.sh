01dc26fb6ca64f182566a61ac6b1adcd3c4b3f22
tenzap <fabstz-it@yahoo.fr>
2022-12-02 11:36:01 +0100

    bash build-scripts: support build with msbuild & force use of xabuild when on linux
    Needed for github actions, and will also support building on macos with
    msbuild

--- src/build-scripts/build-apk.sh
+++ src/build-scripts/build-apk.sh
@@ -3,12 +3,24 @@ set -e
 
 pushd ../keepass2android
 
+# Determine if we use msbuild or xabuild to build.
+if which msbuild > /dev/null; then
+  if [ $(uname) == "Linux" ]; then
+    # For now, when running on Linux, we can't use msbuild but have to use xabuild (provided by https://github.com/xamarin/xamarin-android)
+    BUILDER=xabuild
+  else
+    BUILDER=msbuild
+  fi
+else
+  BUILDER=xabuild
+fi
+
 # check if ANDROID_HOME is defined
 if [ -z ${ANDROID_HOME+x} ];
 then
-	xabuild keepass2android-app.csproj /t:SignAndroidPackage "$@"
+	$BUILDER keepass2android-app.csproj /t:SignAndroidPackage "$@"
 else
-	xabuild keepass2android-app.csproj /p:AndroidSdkDirectory=$ANDROID_HOME /t:SignAndroidPackage "$@"
+	$BUILDER keepass2android-app.csproj /p:AndroidSdkDirectory=$ANDROID_HOME /t:SignAndroidPackage "$@"
 fi
 
 popd
