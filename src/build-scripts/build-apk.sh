ec2030e08de90199c72897cc27e7811384576399
tenzap <fabstz-it@yahoo.fr>
2022-12-02 13:11:03 +0100

    build-apk.sh: fix project name to keepass2android-app.csproj

--- src/build-scripts/build-apk.sh
+++ src/build-scripts/build-apk.sh
@@ -6,9 +6,9 @@ pushd ../keepass2android
 # check if ANDROID_HOME is defined
 if [ -z ${ANDROID_HOME+x} ];
 then
-	xabuild keepass2android.csproj /t:SignAndroidPackage "$@"
+	xabuild keepass2android-app.csproj /t:SignAndroidPackage "$@"
 else
-	xabuild keepass2android.csproj /p:AndroidSdkDirectory=$ANDROID_HOME /t:SignAndroidPackage "$@"
+	xabuild keepass2android-app.csproj /p:AndroidSdkDirectory=$ANDROID_HOME /t:SignAndroidPackage "$@"
 fi
 
 popd
