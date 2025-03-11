d2e0ae6394e2852d9ae02336485d40f3a83bf301
Philipp Crocoll <crocoapps@gmail.com>
2025-03-11 16:02:51 +0100

    after updating to targetSdkVersion=34, an implicit mutable intent could lead to a crash. closes https://github.com/PhilippC/keepass2android/issues/2790

---
+++ src/keepass2android-app/NullActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.Content.PM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keepass2android
+{
+    [Activity(Label = AppNames.AppName,
+        MainLauncher = false,
+        Theme = "@style/Kp2aTheme_BlueNoActionBar",
+        Exported = true)] 
+    ///For autofill, we sometimes need to pass an intent to an inline presentation which never gets fired. We use this as a dummy activity.
+    public class NullActivity: Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            Kp2aLog.Log("NullActivity.OnCreate - this is unexpected.");
+            base.OnCreate(savedInstanceState);
+            Finish();
+        }
+    }
+}
