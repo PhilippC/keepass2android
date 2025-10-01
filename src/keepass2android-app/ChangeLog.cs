c06e8ab2c076f11b8215dcf89fb3c73f907c1504
Philipp Crocoll <crocoapps@gmail.com>
2025-10-01 06:46:50 +0200

    changelog and manifest for 1.15-pre0

--- src/keepass2android-app/ChangeLog.cs
+++ src/keepass2android-app/ChangeLog.cs
@@ -31,8 +31,8 @@ namespace keepass2android
 			MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(ctx);
 			builder.SetTitle(ctx.GetString(Resource.String.ChangeLog_title));
 			List<string> changeLog = new List<string>{
-				
-				BuildChangelogString(ctx, new List<int>
+                BuildChangelogString(ctx, new List<int>{Resource.Array.ChangeLog_1_15}, "1.15"),
+                BuildChangelogString(ctx, new List<int>
                 {
 					Resource.Array.ChangeLog_1_14,
 #if !NoNet
