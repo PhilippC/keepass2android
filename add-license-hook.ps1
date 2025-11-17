b2d24e4ed00066e903ffd5ef2eca1a1afc45898c
Philipp Crocoll <crocoapps@gmail.com>
2025-11-17 11:29:07 +0100

    make addlicense use the GPLv3 file

--- add-license-hook.ps1
+++ add-license-hook.ps1
@@ -1,6 +1,6 @@
 Write-Host "Running addlicense hook..."
 $files = Get-ChildItem -Path src -Recurse -Include *.cs,*.java,*.xml |
-         Where-Object { $_.FullName -notmatch '\\obj\\|\\bin\\|Resources' }
+         Where-Object { $_.FullName -notmatch '\\obj\\|\\bin\\|Resources|bouncycastle|keepassdroid|TwofishCipher|SamsungPass' }
 foreach ($f in $files) {
-    addlicense $f.FullName -c 'Philipp Crocoll' -y
+    addlicense -f GPLv3.txt $f.FullName -c 'Philipp Crocoll' -y
 }
--- src/Kp2aBusinessLogic/IDrawableFactory.cs
+++ src/Kp2aBusinessLogic/IDrawableFactory.cs
@@ -1,3 +1,18 @@
+// This file is part of Keepass2Android, Copyright 2025 Philipp Crocoll.
+//
+//   Keepass2Android is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   Keepass2Android is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with Keepass2Android.  If not, see <http://www.gnu.org/licenses/>.
+
 using Android.Content;
 using Android.Widget;
 using Android.Content.Res;
