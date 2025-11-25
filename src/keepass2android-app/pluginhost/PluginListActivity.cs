87691462b66e4015bc8e59b3f6880a2795abbfda
Philipp Crocoll <crocoapps@gmail.com>
2025-11-25 06:13:02 +0100

    remove event handler for removed button

--- src/keepass2android-app/pluginhost/PluginListActivity.cs
+++ src/keepass2android-app/pluginhost/PluginListActivity.cs
@@ -37,10 +37,6 @@ namespace keepass2android
 					StartActivity(i);
 				};
 
-			FindViewById<Button>(Resource.Id.btnPluginsOnline).Click += delegate
-				{
-					Util.GotoUrl(this, "https://github.com/PhilippC/keepass2android/blob/master/docs/Available-Plug-ins.md");
-				};
 
 		}
 		protected override void OnResume()
